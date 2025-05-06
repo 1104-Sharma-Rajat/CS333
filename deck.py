74a15801abda15734320e389521fae9c785816fe
Rajat Sharma <rajat20151@gmail.com>
2025-05-06 16:15:30 -0700

    restored

--- deck.py
+++ deck.py
@@ -6,3 +6,41 @@
 
 # test
 
+import random
+from card import Card
+
+colors = ['Red', 'Yellow', 'Green', 'Blue']
+values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
+specialCards = ['Wild', 'Wild Draw Four']
+
+class Deck:
+    def __init__(self):
+        self.cards = self.makeDeck()
+        random.shuffle(self.cards)
+
+    def makeDeck(self):
+        cards = []
+        for color in colors:
+            cards.append(Card(color, '0'))
+            for value in values[1:]:
+                cards.append(Card(color, value))
+                cards.append(Card(color, value))
+        for i in range(4):
+            cards.append(Card(None, 'Wild'))
+            cards.append(Card(None, 'Wild Draw Four'))
+        return cards
+
+    def draw(self):
+        if not self.cards:
+            print("Deck is empty!")
+            return None
+        return self.cards.pop()
+
+    def getLength(self):
+        return len(self.cards)
+
+    def getCards(self):
+        return self.cards
+
+    def setCards(self, cards):
+        self.cards = cards
